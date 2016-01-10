ae5457dd4bb0acb64a8d073e5389503c7d167cea
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2016-01-10 23:59:26 +0700

    Fix credentials-dev template to include google key

--- www/application/config/credentials-dev.php
+++ www/application/config/credentials-dev.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$config['google-server-key'] = 'AIzaSyBa1bNBVkchvxSFd8U_Cn7HsHux6M-DIk4';
+$config['scheduled-secret'] = 'kuJ3uQYIHko0';
+
 // E-mail
 $config['email']['protocol'] = 'smtp';
 $config['email']['smtp_host'] = 'smtp-mail.outlook.com';
