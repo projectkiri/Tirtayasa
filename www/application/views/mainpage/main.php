19318e9d35327efb3fe3ecda856de9da435f07ea
William Oktavianus <williamoktavianus1099@gmail.com>
2020-02-24 19:23:01 +0700

    Ubah ke Bootstrap (CSS)

--- www/application/views/mainpage/main.php
+++ www/application/views/mainpage/main.php
@@ -9,105 +9,110 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <meta name="description" content="<?= $this->lang->line('meta-description') ?>" />
         <meta name="author" content="Project Kiri (KIRI)" />
         <meta name="google-site-verification" content="9AtqvB-LWohGnboiTyhtZUXAEcOql9B-8lDjo_wcUew" />
-        <link rel="stylesheet" href="/ext/foundation/css/foundation.min.css" />
+
+ 	    <!-- ganti ke bootstrap -->
+        <link rel="stylesheet" href="/ext/bootstrap-4.4.1-dist/css/bootstrap.min.css" />
+
         <link rel="stylesheet" href="/ext/openlayers/ol.css" />
         <link rel="stylesheet" href="/stylesheets/styleIndex.css" />
         <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
-        <script src="/ext/foundation/js/vendor/modernizr.js"></script>
+        <script src="/ext/bootstrap-4.4.1-dist/js/vendor/modernizr.js"></script>
     </head>
     <body>
-        <div class="row">
-            <div id="controlpanel" class="large-3 large-push-9 columns">
-                <div class="row center">
-                    <img src="/images/kiri200.png" alt="KIRI logo"/>
-                </div>
-                <div class="row">
-                    <div class="small-5 columns">
-                        <select id="regionselect">
-                            <?php foreach ($regions as $key => $value): ?>
-                                <option value="<?= $key ?>"<?= ($region == $key ? ' selected' : '') ?>><?= $value['name'] ?></option>
-                            <?php endforeach; ?>
-                        </select>
+        <div class="container-fluid">
+            <div class="row">
+                <div id="map" class="col-lg-9 ml-auto"></div>
+                <div id="controlpanel" class="col-lg-3">
+                    <div class="row justify-content-md-center">
+                        <img src="/images/kiri200.png" alt="KIRI logo"/>
                     </div>
-                    <div class="small-7 columns">
-                        <select id="localeselect">
-                            <?php foreach ($languages as $key => $value): ?>
-                                <option value="<?= $key ?>"<?= ($locale == $key ? ' selected' : '') ?>><?= $value['name'] ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="small-2 columns">
-                        <label for="startInput" class="inline"><?= $this->lang->line('From') ?>:</label>
-                    </div>
-                    <div class="small-10 columns">
-                        <input type="text" id="startInput" value=""	placeholder="<?= $this->lang->line('placeholder-from') ?>">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="large-12 columns">
-                        <select id="startSelect" class="hidden"></select>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="small-2 columns">
-                        <label for="finishInput" class="inline"><?= $this->lang->line('To') ?>:</label>
-                    </div>
-                    <div class="small-10 columns">
-                        <input type="text" id="finishInput" value="" placeholder="<?= $this->lang->line('placeholder-to') ?>">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="large-12 columns">
-                        <select id="finishSelect" class="hidden"></select>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="small-6 columns">
-                        <a href="#" class="small button expand" id="findbutton"><strong><?= $this->lang->line('Find') ?>!</strong></a>
-                    </div>
-                    <div class="small-3 columns">
-                        <a href="#" class="small button secondary expand" id="swapbutton"><img src="images/swap.png" alt="swap"></a>
-                    </div>
-                    <div class="small-3 columns">
-                        <a href="#" class="small button secondary expand" id="resetbutton"><img src="images/reset.png" alt="reset"></a>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="large-12 columns" id="routingresults">
-                        <div id="results-section-container"></div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="large-12 columns">
-                        <footer>
-                            <a href="<?= $this->lang->line('url-legal') ?>"><?= $this->lang->line('Legal') ?></a> | 
-                            <a href="<?= $this->lang->line('url-feedback') ?>"><?= $this->lang->line('Feedback') ?></a> | 
-                            <a href="<?= $this->lang->line('url-about') ?>"><?= $this->lang->line('About KIRI') ?></a><br/><br/>
-                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                            <input type="hidden" name="cmd" value="_s-xclick">
-                            <input type="hidden" name="hosted_button_id" value="WKWS26A57WHJG">
-                            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                            </form>
-                        </footer>
-                        &nbsp;
-                    </div>
-                </div>
-                <?php if (!is_null($youtube)): ?>
-                    <div class="row">
-                        <div id="youtubepromo" class="reveal-modal small" data-reveal="">
-                            <h3><?= $youtube['label'] ?></h3>
-                            <div class="flex-video">
-                                <iframe width="640" height="480" src="//www.youtube.com/embed/<?= $youtube['code'] ?>" frameborder="0" allowfullscreen></iframe>
-                            </div>
-                            <a class="close-reveal-modal">&#215;</a>
+                    <div class="row p-1">
+                        <div class="col-sm-5">
+                            <select id="regionselect" class="form-control form-control-sm">
+                                <?php foreach ($regions as $key => $value): ?>
+                                    <option value="<?= $key ?>"<?= ($region == $key ? ' selected' : '') ?>><?= $value['name'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="col-sm-7">
+                            <select id="localeselect" class="form-control form-control-sm">
+                                <?php foreach ($languages as $key => $value): ?>
+                                    <option value="<?= $key ?>"<?= ($locale == $key ? ' selected' : '') ?>><?= $value['name'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
                         </div>
                     </div>
-                <?php endif; ?>
+                    <div class="row p-1">
+                        <div class="col-sm-2">
+                            <span for="startInput" class="align-middle"><?= $this->lang->line('From') ?>:</span>
+                        </div>
+                        <div class="col-sm-10">
+                            <input type="text" id="startInput" value=""	placeholder="<?= $this->lang->line('placeholder-from') ?>">
+                        </div>
+                    </div>
+                    <div class="row p-1">
+                        <div class="col-lg-12">
+                            <select id="startSelect" class="hidden"></select>
+                        </div>
+                    </div>
+                    <div class="row p-1">
+                        <div class="col-sm-2">
+                            <span for="finishInput" class="align-middle"><?= $this->lang->line('To') ?>:</span>
+                        </div>
+                        <div class="col-sm-10">
+                            <input type="text" id="finishInput" value="" placeholder="<?= $this->lang->line('placeholder-to') ?>">
+                        </div>
+                    </div>
+                    <div class="row p-1">
+                        <div class="col-lg-12">
+                            <select id="finishSelect" class="hidden"></select>
+                        </div>
+                    </div>
+                    <div class="row p-1">
+                        <div class="col-sm-6">
+                            <a href="#" class="btn btn-primary" id="findbutton"><strong><?= $this->lang->line('Find') ?>!</strong></a>
+                        </div>
+                        <div class="col-sm-3">
+                            <a href="#" class="btn btn-light" id="swapbutton"><img src="images/swap.png" alt="swap"></a>
+                        </div>
+                        <div class="col-sm-3">
+                            <a href="#" class="btn btn-light" id="resetbutton"><img src="images/reset.png" alt="reset"></a>
+                        </div>
+                    </div>
+                    <div class="row p-1">
+                        <div class="col-lg-12" id="routingresults">
+                            <div id="results-section-container"></div>
+                        </div>
+                    </div>
+                    <div class="row p-1">
+                        <div class="col-lg-12">
+                            <footer>
+                                <a href="<?= $this->lang->line('url-legal') ?>"><?= $this->lang->line('Legal') ?></a> | 
+                                <a href="<?= $this->lang->line('url-feedback') ?>"><?= $this->lang->line('Feedback') ?></a> | 
+                                <a href="<?= $this->lang->line('url-about') ?>"><?= $this->lang->line('About KIRI') ?></a><br/><br/>
+                                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                <input type="hidden" name="cmd" value="_s-xclick">
+                                <input type="hidden" name="hosted_button_id" value="WKWS26A57WHJG">
+                                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                                </form>
+                            </footer>
+                            &nbsp;
+                        </div>
+                    </div>
+                    <?php if (!is_null($youtube)): ?>
+                        <div class="row">
+                            <div id="youtubepromo" class="reveal-modal small" data-reveal="">
+                                <h3><?= $youtube['label'] ?></h3>
+                                <div class="flex-video">
+                                    <iframe width="640" height="480" src="//www.youtube.com/embed/<?= $youtube['code'] ?>" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                                <a class="close-reveal-modal">&#215;</a>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                </div>
             </div>
-            <div id="map" class="large-9 large-pull-3 columns"></div>
         </div>
         <script src="/ext/foundation/js/vendor/jquery.js"></script>
         <script src="/ext/foundation/js/vendor/fastclick.js"></script>
