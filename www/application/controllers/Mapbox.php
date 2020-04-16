136dcf2657434eb14e23319606da3b60e8a71f7c
Indra Permana Sugianto <invers273@gmail.com>
2020-04-16 15:25:09 +0700

    Deleting Google Maps

---
+++ www/application/controllers/Mapbox.php
@@ -0,0 +1,26 @@
+<?php 
+    class Mapbox{    
+
+        public function indra(){
+            
+        }
+    
+        public function kelvin(){
+    
+        }
+    
+        public function william(){
+    
+        }
+    
+        public function joshua(){
+
+        }
+
+        public function yoga(){
+
+        }
+
+    }
+
+?>
