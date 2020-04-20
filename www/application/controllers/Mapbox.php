20ecf285307ac49f7f58db8e02ab2f24ebd703b0
Ttanus <kelvinadrian777@gmail.com>
2020-04-20 10:35:47 +0700

    mapbox tester

--- www/application/controllers/Mapbox.php
+++ www/application/controllers/Mapbox.php
@@ -5,4 +5,19 @@ class Mapbox extends CI_Controller {
 	public function pascal() {
 		$this->load->view('mapbox/pascal');
 	}
+	public function indra() {
+		$this->load->view('mapbox/indra');
+	}
+	public function jode() {
+		$this->load->view('mapbox/jode');
+	}
+	public function william() {
+		$this->load->view('mapbox/william');
+	}
+	public function yoga() {
+		$this->load->view('mapbox/yoga');
+	}
+	public function kelvin() {
+		$this->load->view('mapbox/kelvin');
+	}
 }
