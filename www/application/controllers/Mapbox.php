93b553baa036d9e29b4cd06b520a0d5d6ceadba5
Pascal Alfadian Nugroho <pascal@dnartworks.co.id>
2020-04-20 09:47:20 +0700

    Trial: example of showing MapBox map

---
+++ www/application/controllers/Mapbox.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mapbox extends CI_Controller {
+	public function pascal() {
+		$this->load->view('mapbox/pascal');
+	}
+}
