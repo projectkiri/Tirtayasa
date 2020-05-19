996ffdc40311a321bda382eb4641244d807b835f
Pascal Alfadian Nugroho <pascal@unpar.ac.id>
2020-05-19 08:14:52 +0700

    Remove remaining Google Analytics trace

--- www/application/views/mainpage/main.php
+++ www/application/views/mainpage/main.php
@@ -121,22 +121,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</script>
 	<script src="/mainpage/js/protocol.js"></script>
 	<script src="/mainpage/js/main.js?locale=<?= $locale ?>"></script>
-	<script>
-		(function (i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
-			i[r] = i[r] || function () {
-				(i[r].q = i[r].q || []).push(arguments)
-			}, i[r].l = 1 * new Date();
-			a = s.createElement(o),
-			m = s.getElementsByTagName(o)[0];
-			a.async = 1;
-			a.src = g;
-			m.parentNode.insertBefore(a, m)
-		})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-		ga('create', 'UA-36656575-2', 'kiri.travel');
-		ga('require', 'displayfeatures');
-		ga('send', 'pageview');
-	</script>
 </body>
 </html>
